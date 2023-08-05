19a0c7aaaf5777d3087885e4cd0cf0b335d2225d
Milton Salustiano <109237764+miltonneto10@users.noreply.github.com>
2023-08-05 14:43:39 -0300

    Commit Inicial

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld /**para construirmos uma classe, utilizamos a palavra “class”, seguido do nome da classe que queremos utilizar. Tal nome deverá ser o mesmo do arquivo-fonte que criamos**/
+/**O corpo da classe deve ser criado entre chaves (no bloco), nos quais poderemos encontrar os métodos, conforme apresentado no código a seguir:**/
+	public static void main (String[] args) {
+		System.out.println("Olá Mundo!");
+	}//fim do método
+}//fim da classe
+
+/**os métodos definem o tipo de retorno, o nome do método e os parâmetros que esse método recebe, os quais são informados entre parênteses, como pode observar na linha de código apresentada:
+
+						void main (String[] args)
+						
+Nos métodos, há instruções, sendo que cada uma deve ser finalizada com o sinal de ponto e vírgula (;). Este símbolo indica o final da instrução e imprime o texto ou exibe o resultado na tela, como “Olá Mundo!”, no prompt de comandos:
+
+						System.out.println("Olá Mundo!");
+						
+**/
